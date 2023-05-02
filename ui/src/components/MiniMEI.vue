65d6440a98bc0344ca4e9e024d37f2c336923503
Jonathan Crabtree <jonathancrabtree@gmail.com>
2023-05-02 13:13:09 -0400

    Center ME label to reduce overrun.

--- ui/src/components/MiniMEI.vue
+++ ui/src/components/MiniMEI.vue
@@ -236,8 +236,8 @@
       <!-- reference ME axis -->
       <g v-axis="state.me_xaxis" class="xaxis" :transform="`translate(0,${state.me_axis_y})`">
       </g>
-      <text v-if="state.mei.strand == '+'" class="me_label" :x="state.me_xscale(0)" :y="state.me_lbl_y" fill="white" text-anchor="start">{{ state.me_len + " bp " + state.mei.ME + " reference " + me_ref_str + " >>>"}}</text>
-      <text v-else class="me_label" :x="state.me_xscale(0)" :y="state.me_lbl_y" fill="white" text-anchor="end">{{ "<<< " + state.me_len + " bp " + state.mei.ME + me_ref_str}}</text>
+      <text v-if="state.mei.strand == '+'" class="me_label" :x="state.me_xscale(state.me_len/2)" :y="state.me_lbl_y" fill="white" text-anchor="middle">{{ state.me_len + " bp " + state.mei.ME + " reference " + me_ref_str + " >>>"}}</text>
+      <text v-else class="me_label" :x="state.me_xscale(state.me_len/2)" :y="state.me_lbl_y" fill="white" text-anchor="middle">{{ "<<< " + state.me_len + " bp " + state.mei.ME + me_ref_str}}</text>
 
       <!-- match/alignment spans -->
       <polygon v-for="s in state.spans" :points="s.points_str" :fill="pctid_color(s.pct_id, 0.5)" :stroke="pctid_color(s.pct_id, 1.0)" stroke-width="2" />
