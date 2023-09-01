5ef364fff18e7b61dce891740412428ce9badeac
Jonathan Crabtree <jonathancrabtree@gmail.com>
2023-09-01 10:00:05 -0400

    Add more detail to CALU/S-W alignment comparison.

--- ui/src/components/MEI_Report.vue
+++ ui/src/components/MEI_Report.vue
@@ -41,6 +41,8 @@ function update_meis() {
     state.meis.forEach(mei => {
         // me[0], me[1]
         let spans = getAlignmentSpans(mei)
+        mei.n_alignments = spans.length
+
         // type, start, end
         let diffs = getCALUDiffs(mei)
         let deletions = diffs.filter(d => (d.type == 'd'))
@@ -48,7 +50,7 @@ function update_meis() {
         let has_deletions = n_deletions > 0
         
         // check for CALU/LINEU deletion overlapping with a S-W alignment span
-        let has_del_overlap = false
+        let n_del_overlaps = 0
         deletions.forEach(d => {
             if (d['start'] > d['end']) {
                 console.log("deletion has start > end")
@@ -60,16 +62,19 @@ function update_meis() {
                 
                 if ((s.me[0] < d['end']) && (s.me[1] > d['start'])) {
                     //                   console.log("alignment span = " + s.me[0] + "-" + s.me[1] + " d = " + d['start'] + " - " + d['end'])
-                    has_del_overlap = true
+                    n_del_overlaps += 1
                 }
             })
         })
-        
-        // check for CALU/LINEU substitution *not* overlapping with a S-W alignment span
-        let have_orphan_subs = false
+        mei.n_del_overlaps = n_del_overlaps
+
+        // check for CALU/LINEU substitutions *not* overlapping with a S-W alignment span
+        let orphan_subs = []
         let subs = diffs.filter(d => (d.type == 's'))
         let n_subs = subs.length
         let has_subs = n_subs > 0
+        mei.n_subs = n_subs
+
         subs.forEach(sub => {
             let has_span_overlap = false
             spans.forEach(span => {
@@ -78,19 +83,21 @@ function update_meis() {
                 }
             })
             if (!has_span_overlap) {
-                have_orphan_subs = true
+                orphan_subs.push(sub)
             }
         })
         
-        if (has_del_overlap) {
-            //            console.log("overlap in " + mei['chrom'] + ':' + mei['pos'])
+        mei.orphan_subs = orphan_subs
+        mei.n_orphan_subs = mei.orphan_subs.length
+
+        if (n_del_overlaps > 0) {
             n_with_overlap += 1
         }
-        if (have_orphan_subs) {
-            //           console.log("orphan subs in " + mei['chrom'] + ':' + mei['pos'])
+        if (mei.n_orphan_subs > 0) {
             n_with_orphan_subs += 1
             orphan_subs_meis.push(mei)
         }
+        // whether the MEI has *any* CALU deletions or substitutions, respectively
         if (has_deletions) {
             n_with_deletion += 1 
         }
@@ -139,6 +146,8 @@ state.meis = props.meis
                             <div v-for="(mei, m) in state.orphan_subs_meis" class="pt-3">
                                 <span style="font-weight: bold; font-size: 1rem;">{{ m+1 }} / {{  state.orphan_subs_meis.length }}</span>
                                 <MEI :mei="mei" :key="mei.key" :label="(m + 1) + '/' + state.orphan_subs_meis.length" />
+                                CALU/LINEU-called substitutions not covered by any pipeline S-W alignment: {{ mei.n_orphan_subs }} / {{ mei.n_subs }} {{ "[" + mei.orphan_subs.map(s => s.text).join(",") + "]" }}<br clear="both">
+                                Pipeline S-W alignments that overlap with a CALU/LINEU-called deletion: {{ mei.n_del_overlaps }} / {{ mei.n_alignments }}<br clear="both">
                                 {{ mei.ME_diffs }}
                             </div>
                             
