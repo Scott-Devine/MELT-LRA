5458d32f342e0663ecdf8b5f82b204bd0a7aab85
Jonathan Crabtree <jonathancrabtree@gmail.com>
2023-10-18 16:35:08 -0400

    Modify viewer to use inline uuencoded data.

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,7 +2,11 @@
   import { ref, computed, reactive, watch } from 'vue';
   import MEI_Viewer from './components/MEI_Viewer.vue'
   import CALU_Report from './components/CALU_Report.vue'
+  import { getData } from './inline_data.js'
 
+  // retrieve inline data
+  const data_files = getData();
+  
   const csv_headers = ['chrom', 'pos', 'strand', 'ME', '%ME', '%id', '%id_ng', '%cov', 'insertion_seq', 'left_flank_seq', 'right_flank_seq', 'TSD_seq', 'polyX_coords', 'ME_coords', 'insertion_coords', 'match_string',
             'ME_family', 'ME_subfamily', 'ME_start', 'ME_stop', 'ME_num_diag_matches', 'ME_num_diffs', 'ME_diffs', 'overlapping_annots', 'genotype', 'hap1_region', 'hap2_region']
 
@@ -40,44 +44,33 @@
     return mei
   }
 
-  // hard-coded data sources
-  const mei_files = [
-    // HG00514 chrY only - DEBUG
-    "assets/data/HG00514-chr22-PAV-MEs-v1.3.0-90-90-95bp.csv",
-    // HG00514 all
-    "assets/data/HG00514-PAV-MEs-v1.3.0-90-90-95bp.csv",
-    // HG00514 all but chrY
-//    "assets/data/HG00514-CCS-PAV-MEs-v1.2.0-90-90-95bp.csv",
-    // 2023-08-23 5 new samples
-    "assets/data/HG00171-PAV-MEs-v1.3.0-90-90-95bp.csv",
-    "assets/data/HG00733-PAV-MEs-v1.3.0-90-90-95bp.csv",
-    "assets/data/HG02666-PAV-MEs-v1.3.0-90-90-95bp.csv",
-    "assets/data/HG02953-PAV-MEs-v1.3.0-90-90-95bp.csv",
-    "assets/data/NA21487-PAV-MEs-v1.3.0-90-90-95bp.csv"
-  ]
-
-  const mei_urls = mei_files.map(f => { return new URL(f, import.meta.url).href })
+//  const mei_files = data_files.map(df => { df.file });
+//  const mei_urls = mei_files.map(f => { return new URL(f, import.meta.url).href })
     
   const state = reactive({
-    'mei_urls': mei_urls,
-    'selected_mei_url': null,
+    'data_files': data_files,
+    'selected_data_file': null,
     'meis': [],
     'tab': "viewer"
   })
 
-  watch(() => state.selected_mei_url, (newValue) => {
-    fetch(newValue)
-    .then(res => {
-      return res.text()
-    }).then(txt => {
-      state.meis = txt.split("\n").slice(1).filter(l => !l.match(/^\s*$/)).map(ms => parse_mei(ms))
-    })
-    .catch(err => {
-      console.log("caught error " + err)
-    })
+//  watch(() => state.selected_mei_url, (newValue) => {
+//    fetch(newValue)
+//    .then(res => {
+//      return res.text()
+//    }).then(txt => {
+//      state.meis = txt.split("\n").slice(1).filter(l => !l.match(/^\s*$/)).map(ms => parse_mei(ms))
+//    })
+//    .catch(err => {
+//      console.log("caught error " + err)
+//    })
+//  })
+
+  watch(() => state.selected_data_file, (df) => {
+    state.meis = df.data.split("\n").slice(1).filter(l => !l.match(/^\s*$/)).map(ms => parse_mei(ms))
   })
 
-  state.selected_mei_url = mei_urls[0]
+  state.selected_data_file = data_files[0]
 
 </script>
 
@@ -87,7 +80,7 @@
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>
         MEI Callset: 
-        <v-menu><template v-slot:activator="{ props }"><v-chip size="large" v-bind="props">{{state.selected_mei_url}}</v-chip></template><v-list v-model="state.mei_list"><v-list-item v-for="(i, ind) in state.mei_urls" @click="state.selected_mei_url = i"><v-list-item-title>{{i}}</v-list-item-title></v-list-item></v-list></v-menu>
+        <v-menu><template v-slot:activator="{ props }"><v-chip size="large" v-bind="props">{{state.selected_data_file.file}}</v-chip></template><v-list><v-list-item v-for="(i, ind) in state.data_files" @click="state.selected_data_file = i"><v-list-item-title>{{i.file}}</v-list-item-title></v-list-item></v-list></v-menu>
       </v-toolbar-title>
       <template v-slot:extension>
         <v-tabs v-model="state.tab">
